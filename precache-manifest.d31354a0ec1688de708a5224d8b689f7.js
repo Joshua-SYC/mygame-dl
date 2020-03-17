082a6fe65791f3d3e53255989476b44e1db9a16b
Joshua <624891117@qq.com>
2020-03-17 16:49:40 +0800

    Updates

---
+++ precache-manifest.d31354a0ec1688de708a5224d8b689f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87e5db928a1a6016e00ac1114744304",
+    "url": "/mygame-dl/index.html"
+  },
+  {
+    "revision": "4d91abc0f50d32caf1c5",
+    "url": "/mygame-dl/static/css/main.5dc11178.chunk.css"
+  },
+  {
+    "revision": "14847b02e9a461d5b819",
+    "url": "/mygame-dl/static/js/0.4855464d.chunk.js"
+  },
+  {
+    "revision": "a5265bc37eb4a4fa703f",
+    "url": "/mygame-dl/static/js/3.aa389b53.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mygame-dl/static/js/3.aa389b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689a03349c10f6039db1",
+    "url": "/mygame-dl/static/js/4.eccc940a.chunk.js"
+  },
+  {
+    "revision": "3e3a31a89c923ae1f604",
+    "url": "/mygame-dl/static/js/5.9f63f9d5.chunk.js"
+  },
+  {
+    "revision": "4d91abc0f50d32caf1c5",
+    "url": "/mygame-dl/static/js/main.14207cd5.chunk.js"
+  },
+  {
+    "revision": "64fdffbba6f43c8ed704",
+    "url": "/mygame-dl/static/js/runtime-main.c20a4a4c.js"
+  }
+]);
